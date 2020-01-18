b1aea9c70aea40c74bfeba34b7bd00bac57d414c
Jobin Thomas <jobinthomas1610@gmail.com>
2020-01-18 11:28:20 +0000

    Zull proxy configured

---
+++ src/test/java/com/capstone/zuul/CapstoneZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
