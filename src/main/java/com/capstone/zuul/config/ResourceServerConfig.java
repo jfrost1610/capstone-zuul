6cf1899010ee82dc6d8a35982e363893a19d9161
Jobin Thomas <jobinthomas1610@gmail.com>
2020-01-20 16:11:56 +0000

    Added OAuth2 Resource server config

---
+++ src/main/java/com/capstone/zuul/config/ResourceServerConfig.java
@@ -0,0 +1,49 @@
+package com.capstone.zuul.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+@Configuration
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Autowired
+	private DataSource datasource;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.oauth2.config.annotation.web.configuration.
+	 * ResourceServerConfigurerAdapter#configure(org.springframework.security.oauth2
+	 * .config.annotation.web.configurers.ResourceServerSecurityConfigurer)
+	 */
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.resourceId("zuul_resource").tokenStore(getTokenStore());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.oauth2.config.annotation.web.configuration.
+	 * ResourceServerConfigurerAdapter#configure(org.springframework.security.config
+	 * .annotation.web.builders.HttpSecurity)
+	 */
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated();
+	}
+
+	@Bean
+	public TokenStore getTokenStore() {
+		return new JdbcTokenStore(datasource);
+	}
+
+}
